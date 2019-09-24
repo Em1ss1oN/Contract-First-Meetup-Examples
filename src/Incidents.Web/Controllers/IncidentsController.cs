fbc84936e88d58c5aede998d579c70146b457618
Nikolay Mikhailychev <nmikhailychev@ptsecurity.com>
2019-09-24 15:16:47 +0300

    fixture

--- src/Incidents.Web/Controllers/IncidentsController.cs
+++ src/Incidents.Web/Controllers/IncidentsController.cs
@@ -3,25 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using AutoFixture;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Incidents.Web.Controllers
 {
     public class IncidentsController : IncidentsControllerBase
     {
+        private Fixture _fixture = new Fixture();
+
         public override Task<IncidentListDto> GetAll(int? limit, int? offset, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            _fixture.Customize<IncidentListDto>(c =>
+                c.With(dto => dto.Items, () => _fixture.CreateMany<IncidentListItemDto>(offset ?? 100).ToList()));
+            return Task.FromResult(_fixture.Create<IncidentListDto>());
         }
 
         public override Task<ObjectId> Create(IncidentModel request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(_fixture.Create<ObjectId>());
         }
 
         public override Task<IncidentDto> Get(Guid id, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            _fixture.Customize<IncidentDto>(c =>
+                c.With(dto => dto.Id, () => id));
+
+            return Task.FromResult(_fixture.Create<IncidentDto>());
         }
     }
 }
